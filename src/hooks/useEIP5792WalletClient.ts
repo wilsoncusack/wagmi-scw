8df2169cad62c008084ec3c1935afc9c25d70d9a
Wilson Cusack <wilson.cusack@coinbase.com>
2024-04-30 12:05:30 -0400

    add hook for useEIP5792WalletClient

---
+++ src/hooks/useEIP5792WalletClient.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import { WalletClient } from 'viem'
+import { type Config, type UseWalletClientParameters, type UseWalletClientReturnType, useWalletClient, ResolvedRegister } from 'wagmi'
+import { WalletActionsEip5792, walletActionsEip5792 } from 'viem/experimental'
+import { GetWalletClientData } from 'wagmi/query'
+
+type EIP5792WalletClient = WalletClient & WalletActionsEip5792;
+
+export function useEIP5792WalletClient<
+config extends Config = ResolvedRegister['config'],
+chainId extends config['chains'][number]['id'] = config['chains'][number]['id'],
+selectData = GetWalletClientData<config, chainId> & WalletActionsEip5792,
+>(
+parameters: UseWalletClientParameters<config, chainId, selectData> = {},
+): UseWalletClientReturnType<config, chainId, selectData> {
+  const {data: originalWalletClient, ...rest } = useWalletClient()
+  const [walletClient, setWalletClient] = useState<EIP5792WalletClient | undefined>(undefined);
+  
+  useEffect(() => {
+    if (originalWalletClient) {
+      const client = originalWalletClient.extend(walletActionsEip5792());
+      setWalletClient(client)
+    }
+  }, [originalWalletClient])
+
+  return {data: walletClient , ...rest} as any;
+}
--- src/hooks/useWalletCapabilities.ts
+++ src/hooks/useWalletCapabilities.ts
@@ -2,22 +2,24 @@ import { useEffect, useState } from 'react'
 import { WalletCapabilities } from 'viem'
 import { useWalletClient } from 'wagmi'
 import { walletActionsEip5792 } from 'viem/experimental'
+import { useEIP5792WalletClient } from './useEIP5792WalletClient'
 
 function useWalletCapabilities({chainId} : {chainId?: number}) {
-  const { data: walletClient } = useWalletClient()
-  const [capabilities, setCapabilities] = useState<{ [chainId: number]: WalletCapabilities } | null>(null)
+  const { data: walletClient } = useEIP5792WalletClient()
+  const [capabilities, setCapabilities] = useState<{ [chainId: number]: WalletCapabilities }>({})
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     if (walletClient) {
-      const client = walletClient.extend(walletActionsEip5792())
-      client.getCapabilities().then((capabilities) => {
+      walletClient.getCapabilities()
+      .then((capabilities) => {
         setCapabilities(capabilities)
         setLoading(false)
       })
-    } else {
-      setLoading(false)
-    }
+      .catch((e) => {
+        setLoading(false)
+      })
+    } 
   }, [walletClient])
 
   return { capabilities: capabilities && chainId ? capabilities[chainId] : capabilities, loading }
