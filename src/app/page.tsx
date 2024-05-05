ac772ffbc357d0bdc6eca687c576166520665c04
Wilson Cusack <wilson.cusack@coinbase.com>
2024-05-05 15:23:42 -0400

    use wagmi hooks + add sign examples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
-import useWalletCapabilities from '@/hooks/useWalletCapabilities'
-import { useWriteContracts } from 'wagmi/experimental'
-import { useCallsStatus } from '@/hooks/useCallsStatus'
-import { useState } from 'react'
+import { Capabilities } from '@/components/Capabilities'
+import { Transact } from '@/components/Transact'
+import { SignMessage } from '@/components/SignMessage'
+import { TypedSign } from '@/components/TypedSign'
 
 const abi = [
 	{
@@ -20,20 +20,15 @@ function App() {
   const account = useAccount()
   const { connectors, connect, status, error } = useConnect()
   const { disconnect } = useDisconnect()
-  const { capabilities } = useWalletCapabilities({ chainId: account.chainId })
-  const [id, setId] = useState<string | undefined>(undefined)
-  const { writeContracts } = useWriteContracts({mutation: {onSuccess: (id) => setId(id)}})
-  const {data: callsStatus} = useCallsStatus({id})
 
   return (
     <>
       <div>
         <h2>Account</h2>
-
         <div>
           status: {account.status}
           <br />
-          capabilities: {capabilities && JSON.stringify(capabilities)}
+          <Capabilities />
           <br />
           addresses: {JSON.stringify(account.addresses)}
           <br />
@@ -62,28 +57,10 @@ function App() {
         <div>{error?.message}</div>
       </div>
       { account.address && 
-      <div> 
-        <h2>Transact</h2>
-        <div>
-          <button onClick={() => {
-          writeContracts({
-            contracts: [{
-              address: "0x119Ea671030FBf79AB93b436D2E20af6ea469a19",
-              abi,
-              functionName: "safeMint",
-              args: [account.address],
-            }, {
-              address: "0x119Ea671030FBf79AB93b436D2E20af6ea469a19",
-              abi,
-              functionName: "safeMint",
-              args: [account.address],
-            }],
-          
-          })
-          }}>Mint</button>
-          {id && <div> ID: {id}</div>}
-          {callsStatus && <div> Status: {callsStatus.status}</div>}
-        </div>
+      <div>
+      <Transact/>
+      <SignMessage/>
+      <TypedSign/>
       </div>
       }
     </>
---
+++ src/components/SignMessage.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { Hex } from "viem";
+import { useAccount, usePublicClient, useSignMessage } from "wagmi";
+import { SiweMessage } from "siwe";
+
+export function SignMessage() {
+  const account = useAccount()
+  const client = usePublicClient()
+  const [signature, setSignature] = useState<Hex | undefined>(undefined)
+  const {signMessage} = useSignMessage({mutation: {onSuccess: (sig) => setSignature(sig)}});
+  const message = useMemo(() => {
+    return new SiweMessage({
+      domain: document.location.host,
+      address: account.address,
+      chainId: account.chainId,
+      uri: document.location.origin,
+      version: "1",
+      statement: "Smart Wallet SIWE Example",
+      nonce: "12345678"
+    })
+  }, []);
+
+  const [valid, setValid] = useState<boolean | undefined>(undefined)
+
+  const checkValid = useCallback(async () =>{
+    if (!signature || !account.address) return
+
+    client.verifyMessage({
+      address: account.address,
+      message: message.prepareMessage(),
+      signature,
+    }).then((v) => setValid(v))
+  }, [signature, account])
+
+  useEffect(() => {
+    checkValid()
+  }, [signature, account])
+
+  return(
+    <div>
+      <h2>Sign Message (Sign In with Ethereum)</h2>
+      <button onClick={() => signMessage({message: message.prepareMessage()})}>
+        Sign
+      </button>
+      <p>{}</p>
+      {signature && <p>Signature: {signature}</p>}
+      {valid != undefined && <p> Is valid: {valid.toString()} </p>}
+    </div>
+  )
+}
