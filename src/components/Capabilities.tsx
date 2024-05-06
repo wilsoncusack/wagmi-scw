46878df83b4bdd73f372ad5ebbda56214b2519a9
Wilson Cusack <wilson.cusack@coinbase.com>
2024-05-06 15:24:09 -0400

    fix

--- src/components/Capabilities.tsx
+++ src/components/Capabilities.tsx
@@ -4,7 +4,7 @@ import { useCapabilities } from "wagmi/experimental";
 
 export function Capabilities() {
   const account = useAccount()
-  const {data: capabilities} = useCapabilities({ chainId: account.chainId })
+  const {data: capabilities} = useCapabilities({account: account.address})
 
   return (
     <p>
