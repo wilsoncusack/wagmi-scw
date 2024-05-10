1db2de9a7494835d73bbc4f3c903ed9feac2ff61
Wilson Cusack <wilson.cusack@coinbase.com>
2024-05-10 10:36:40 -0400

    fixes

--- src/app/api/paymaster/route.ts
+++ src/app/api/paymaster/route.ts
@@ -80,27 +80,34 @@ function willSponsor({
   entrypoint,
   userOp,
 }: { chainId: number; entrypoint: string; userOp: UserOperation<"v0.6"> }) {
+  console.log(0)
   if (chainId !== baseSepolia.id) return false;
-  if (entrypoint !== ENTRYPOINT_ADDRESS_V06) return false;
+  console.log(0.1)
+  if (entrypoint.toLowerCase() !== ENTRYPOINT_ADDRESS_V06.toLowerCase()) return false;
+  console.log(0.2)
 
   try {
     const calldata = decodeFunctionData({
       abi: coinbaseSmartWalletABI,
       data: userOp.callData,
     });
+    console.log(calldata.args)
 
     // keys.coinbase.com always uses executeBatch
+    console.log(1)
     if (calldata.functionName !== "executeBatch") return false;
-
+    console.log(2)
     if (!calldata.args || calldata.args.length == 0) return false;
 
-    const calls = calldata.args as {
+    const calls = calldata.args[0] as {
       target: Address;
       value: bigint;
       data: Hex;
     }[];
+    console.log(3)
     if (calls.length > 2) return false;
 
+    console.log(4)
     let callToCheckIndex = 0;
     if (calls.length == 2) {
       // if there are two calls, the first should be Magic Spend
@@ -108,18 +115,19 @@ function willSponsor({
         return false;
       callToCheckIndex = 1;
     }
-
+    console.log(5)
+    console.log(calls[callToCheckIndex])
     if (
       calls[callToCheckIndex].target.toLowerCase() !==
       myNFTAddress.toLowerCase()
     )
       return false;
-
+      console.log(6)
     const innerCalldata = decodeFunctionData({
       abi: myNFTABI,
       data: calls[callToCheckIndex].data,
     });
-
+    console.log(7)
     if (innerCalldata.functionName !== "safeMint") return false;
 
     return true;
