82a190a776a8e84d6e2a8942fa62c97a5d657708
Wilson Cusack <wilson.cusack@coinbase.com>
2024-08-03 14:28:04 -0400

    debug

--- src/app/api/paymaster/route.ts
+++ src/app/api/paymaster/route.ts
@@ -5,12 +5,13 @@ export async function POST(r: Request) {
   const req = await r.json();
   const method = req.method;
   const [userOp, entrypoint, chainId] = req.params;
-
+  console.log(req.params)
   const sponsorable = await willSponsor({
     chainId: parseInt(chainId),
     entrypoint,
     userOp,
   });
+  console.log("sponsorable", sponsorable)
   if (!sponsorable) {
     return Response.json({ error: "Not a sponsorable operation" });
   }
