40daf805ce7940aa96ad1f1a5e831e70fbb4ee4c
Wilson Cusack <wilson.cusack@coinbase.com>
2024-08-03 14:36:50 -0400

    fix chain

--- src/app/api/config.ts
+++ src/app/api/config.ts
@@ -1,16 +1,16 @@
 import { createClient, createPublicClient, http } from "viem";
-import { baseSepolia } from "viem/chains";
+import { base, baseSepolia } from "viem/chains";
 import { ENTRYPOINT_ADDRESS_V06 } from "permissionless";
 import { paymasterActionsEip7677 } from "permissionless/experimental";
 
 export const client = createPublicClient({
-  chain: baseSepolia,
+  chain: base,
   transport: http(),
 });
 
 const paymasterService = process.env.PAYMASTER_SERVICE_URL!;
 
 export const paymasterClient = createClient({
-  chain: baseSepolia,
+  chain: base,
   transport: http(paymasterService),
 }).extend(paymasterActionsEip7677({ entryPoint: ENTRYPOINT_ADDRESS_V06 }));
